3c44e6462de5d85255f25a92dc9b8b5e8bfab9ed
lootverse <davidsigmakul@gmail.com>
2026-07-21 21:18:12 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-let prod='',harga='';function openOrder(p,h){prod=p;harga=h;info.innerHTML='<b>'+p+'</b><br>'+h;popup.style.display='flex';}
-function closePop(){popup.style.display='none';}
-function sendOrder(){if(!user.value||!pass.value){alert('Isi username dan password');return;}
-let msg=`Halo Admin LootVerse! 👋
-
-Saya ingin Top Up Robux Via Login.
-
-👤 Username/Email : ${user.value}
-🔒 Password : ${pass.value}
-💎 Paket : ${prod}
-💰 Harga : ${harga}
-💳 Pembayaran : ${pay.value}`;
-window.open('https://wa.me/6289674970911?text='+encodeURIComponent(msg),'_blank');}
+let type='',prod='',price='';
+function orderGP(p,h){type='gp';prod=p;price=h;title.innerText=p+' - '+h;pass.style.display='none';pop.style.display='flex';}
+function orderRB(p,h){type='rb';prod=p;price=h;title.innerText=p+' - '+h;pass.style.display='block';pop.style.display='flex';}
+function closePop(){pop.style.display='none';}
+function send(){if(!user.value)return alert('Isi username');if(type=='rb'&&!pass.value)return alert('Isi password');let msg=`Halo Admin LootVerse! 👋
+👤 Username: ${user.value}
+${type=='rb'?'🔒 Password: '+pass.value+'\n':''}📦 Produk: ${prod}
+💰 Harga: ${price}
+💳 Pembayaran: ${pay.value}`;window.open('https://wa.me/6289674970911?text='+encodeURIComponent(msg));}
